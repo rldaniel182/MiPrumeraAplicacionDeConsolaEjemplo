a371c75b5eb05a365f3101dd793a91a25d039169
rldaniel182 <rldaniel182@gmail.com>
2021-11-02 16:42:34 -0600

    Este es el comienzo del proyecto

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            List<string> listatest = new List<string>();
+            listatest.Add("1");
+            listatest.Add("2");
+            listatest.Add("3");
+            listatest.Add("4");
+            listatest.Add("6");
+            listatest.Add("6");
+            listatest.Add("7");
+
+            string combindedString = string.Join(",", listatest);
+            Console.WriteLine(combindedString);
+            Console.ReadLine();
+        }
+    }
+}
